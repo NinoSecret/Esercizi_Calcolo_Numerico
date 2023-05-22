104536fbd50e1c990f8214f33b191e5990451d0e
Vitaliy Lyaskovskiy <125906345+lyasv95@users.noreply.github.com>
2023-05-22 15:17:59 +0200

    eser

--- gruppo-2/g2e2.m
+++ gruppo-2/g2e2.m
@@ -1,36 +1,39 @@
+function [] = g2e2(a,b,m,n)
+%UNTITLED3 Summary of this function goes here
+%   Detailed explanation goes here
 
 % Inizializzazione delle variabili
-a = -1; % limite inferiore dell'intervallo
-b = 1;  % limite superiore dell'intervallo
+% a limite inferiore dell'intervallo
+% b limite superiore dell'intervallo
 
 % Inizializzazione dei vettori per memorizzare i risultati
-norm1_results = zeros(1,19);
-norm2_results = zeros(1,19);
-normInf_results = zeros(1,19);
+norm1_results = zeros(1,n-1);
+norm2_results = zeros(1,n-1);
+normInf_results = zeros(1,n-1);
 
 % Calcolo delle norme per vettori di dimensioni diverse
-for n = 2:20
+for i = m:n
     % Creazione del vettore con n punti equidistanti nell'intervallo [a, b]
-    v = linspace(a, b, n);
+    v = linspace(a, b, i);
     
     % Calcolo della norma-1 (somma dei valori assoluti)
     norm1 = sum(abs(v));
-    norm1_results(n-1) = norm1;
+    norm1_results(i-1) = norm1;
     
     % Calcolo della norma-2 (radice quadrata della somma dei quadrati)
     norm2 = sqrt(sum(v.^2));
-    norm2_results(n-1) = norm2;
+    norm2_results(i-1) = norm2;
     
     % Calcolo della norma-infinito (valore massimo assoluto)
     normInf = max(abs(v));
-    normInf_results(n-1) = normInf;
+    normInf_results(i-1) = normInf;
 end
 
 % Creazione del grafico
 figure;
-plot(2:20, norm1_results, 'r', 'LineWidth', 2); hold on;
-plot(2:20, norm2_results, 'g', 'LineWidth', 2); 
-plot(2:20, normInf_results, 'b', 'LineWidth', 2); 
+plot(m:n, norm1_results, 'r', 'LineWidth', 2); hold on;
+plot(m:n, norm2_results, 'g', 'LineWidth', 2); 
+plot(m:n, normInf_results, 'b', 'LineWidth', 2); 
 
 % Aggiunta di legenda e titoli
 legend('Norma-1', 'Norma-2', 'Norma-Infinito', 'Location', 'northwest');
@@ -39,3 +42,6 @@ xlabel('Dimensione del vettore (n)');
 ylabel('Valore della Norma');
 grid on;
 hold off;
+
+end
+
