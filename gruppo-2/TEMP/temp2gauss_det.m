bd304ef55526ab549a11e9d65be2240bb2d75fff
Vitaliy Lyaskovskiy <125906345+lyasv95@users.noreply.github.com>
2023-05-23 11:26:38 +0200

    test

---
+++ gruppo-2/TEMP/temp2gauss_det.m
@@ -0,0 +1,35 @@
+function detA = gauss_det(A)
+    % Controlla se la matrice è quadrata
+    [n, m] = size(A);
+    if n ~= m
+        error('La matrice deve essere quadrata');
+    end
+
+    detA = 1;
+    for i = 1:n
+        % Trova il massimo in valore assoluto nella colonna i da i a n
+        [pivot, idx] = max(abs(A(i:n,i)));
+        idx = idx + i - 1; % Calcola l'indice corretto
+
+        % Se il pivot è zero, la matrice è singolare
+        if pivot == 0
+            detA = 0;
+            return;
+        end
+
+        % Scambia le righe se necessario
+        if idx ~= i
+            A([i idx], :) = A([idx i], :);
+            detA = -detA;
+        end
+
+        % Riduci le altre righe
+        for j = i+1:n
+            factor = A(j,i) / A(i,i);
+            A(j,:) = A(j,:) - factor * A(i,:);
+        end
+
+        % Moltiplica il determinante per il pivot
+        detA = detA * A(i,i);
+    end
+end
