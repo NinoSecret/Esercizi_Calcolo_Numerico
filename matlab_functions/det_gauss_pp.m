f7a62da72a2fbf771c18f386b86f8053734cc72f
Davide Ferrara <davide98ferrara@gmail.com>
2023-05-24 18:22:34 +0200

    sistemato gauss pp

--- matlab_functions/det_gauss_pp.m
+++ matlab_functions/det_gauss_pp.m
@@ -1,39 +1,36 @@
 function [U, deter]=det_gauss_pp(A)
-n=size(A,1);
-U=A;
-deter=1;
+n = size(A,1);
+U = A;
+deter = 1;
 
-for k=1:1:n-1
-    % PIVOTING
-    [piv, j]=max(abs(A(k:n,k))); 
-    
-    % CASO DETERMINANTE 0
+for k = 1: n-1
+    % Pivoting
+    [piv, j] = max(abs(A(k:n,k))); 
+    % Determinante uguale a zero
     if (piv == 0) 
-        deter=0;
+        deter = 0;
         return
     end
-    % SCAMBIO RIGHE
-    if (j ~=1) 
-        temp=A(j+k-1,:);
-        A(j+k-1,:)=A(k,:);
-        A(k,:)=temp;
-        deter=-deter;
+    % Scambio le righe e cambio il segno del determinante
+    if (j ~= 1) 
+        temp = A(j+k-1,:);
+        A(j+k-1,:) = A(k,:);
+        A(k,:) = temp;
+        deter = -deter;
     end
-
-    % deter=deter*A(k,k);
-
-    % ELIMINAZIONE GAUSSIANA.
-    for index=k+1:1:n
-        m(index,k)=A(index,k)/A(k,k);
-        A(index,k)=0;
-        for j=k+1:1:n
-            A(index,j)=A(index,j)-m(index,k)*A(k,j);
+    
+    % Gauss Elimination
+    for i = k+1 : n
+        quot = A(i,k) / A(k,k);
+        A(i,k) = 0;
+        for j = k+1 : n
+            A(i,j) = A(i,j)- quot *A(k,j);
         end
     end
+    
 end
 
 U=A;
-% CALCOLO DETERMINANTE
+% Calcolo il detrminante
 det_U=prod(diag(U));
-% MATRICE FINALE "U"
 deter=deter*det_U;
