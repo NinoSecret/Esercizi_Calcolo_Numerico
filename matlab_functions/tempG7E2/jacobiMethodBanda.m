69e52269f9fccb295e7c69aedcef22916c5985a1
Vitaliy Lyaskovskiy <125906345+lyasv95@users.noreply.github.com>
2023-05-29 16:33:38 +0200

    update

--- matlab_functions/tempG7E2/jacobiMethodBanda.m
+++ matlab_functions/tempG7E2/jacobiMethodBanda.m
@@ -1,22 +1,25 @@
 function x = jacobiMethodBanda(diag_main, diag_upper1, diag_upper2, diag_upper3, diag_lower1, diag_lower2, diag_lower3, b)
     % dimensione del sistema
-    n = length(b);
+    n = length(b);  % La lunghezza del vettore b determina la dimensione del sistema lineare.
     
     % inizializzazione del vettore soluzione
-    x = zeros(n, 1);
+    x = zeros(n, 1);  % Inizializza la soluzione x come un vettore colonna di zeri.
     
     % criterio di arresto
-    eps = 1e-6;
+    eps = 1e-6;  % La precisione desiderata. L'algoritmo si ferma quando la differenza tra le soluzioni di due iterazioni successive è inferiore a eps.
     
     % numero massimo di iterazioni
-    max_iterations = 1000;
+    max_iterations = 1000;  % L'algoritmo si ferma anche se non ha raggiunto la precisione desiderata dopo max_iterations iterazioni.
     
+    % Iterazioni di Jacobi
     for k = 1:max_iterations
-        x_old = x;
+        x_old = x;  % Salva la soluzione corrente prima di calcolare la nuova.
         
+        % Calcolo della nuova soluzione
         for i = 1:n
-            sum_term = 0;
+            sum_term = 0;  % Inizializza la somma dei termini non diagonali.
             
+            % Calcolo della somma dei termini non diagonali
             if i > 1
                 sum_term = sum_term + diag_lower1(i-1) * x_old(i-1);
             end
@@ -36,11 +39,12 @@ function x = jacobiMethodBanda(diag_main, diag_upper1, diag_upper2, diag_upper3,
                 sum_term = sum_term + diag_upper3(i) * x_old(i+3);
             end
             
+            % Calcolo del nuovo termine x(i) della soluzione
             x(i) = (b(i) - sum_term) / diag_main(i);
         end
         
         % Controllo del criterio di arresto
-        if norm(x - x_old, inf) < eps
+        if norm(x - x_old, inf) < eps  % Se la norma infinito (la massima differenza assoluta tra le componenti) tra x e x_old è inferiore a eps, l'algoritmo si ferma.
             break;
         end
     end
